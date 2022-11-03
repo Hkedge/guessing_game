b766349920d5908e9a3040aabeb81977eb7a4ab0
Hkedge <hollyekedge@gmail.com>
2022-11-02 21:43:29 -0600

    Small changes

--- js/guessing-game-specs.js
+++ js/guessing-game-specs.js
@@ -191,3 +191,4 @@ describe('newGame function', () => {
     });
   });
 });
+
--- js/guessing-game.js
+++ js/guessing-game.js
@@ -27,23 +27,23 @@ let submittedAnswersArray = [];
     clearGreen.className = "ans";
     let clearYellow = document.getElementsByClassName("yellow");
     clearYellow.className = "ans";
-    let submittedAnswersArray = []
+    let submittedAnswersArray = [];
     return correctAnswer;
 } 
 
-let reset = document.getElementById("playAgain")
-reset.addEventListener('click', resetGame)
+let reset = document.getElementById("playAgain");
+reset.addEventListener('click', resetGame);
 
 let whereToLog = document.getElementsByClassName("ans");
-let whereToChange = document.getElementsByClassName('ans')
-let result = document.getElementById("result")
+let whereToChange = document.getElementsByClassName('ans');
+let result = document.getElementById("result");
 
-let form = document.getElementById("addGuess")
+let form = document.getElementById("addGuess");
 // console.log(form)
 
 //I could not get this code to work it will not even console.log the value
-let submittedValue = document.getElementById("guess").value
-console.log(submittedValue)
+let submittedValue = document.getElementById("guess").value;
+console.log(submittedValue);
 
 form.addEventListener('submit', pushAnswers);
 
@@ -59,53 +59,52 @@ function pushAnswers(submittedValue){
 /* Since i could not get the returned numbers from the form I added these to push in answer so you can see the
 other code and how it works if you uncomment them one at a time starting from the top you can see the page change
 because of this I also can't really test to see if the game reset works*/
-// pushAnswers(72)
-// pushAnswers(63)
-// pushAnswers(10)
-// pushAnswers(20)
-// pushAnswers(31)
-// pushAnswers(30)
-// pushAnswers(15)
+// pushAnswers(72);
+// pushAnswers(63);
+// pushAnswers(10);
+// pushAnswers(20);
+// pushAnswers(31);
+// pushAnswers(30);
+// pushAnswers(15);
 
-
-logAnswer(submittedAnswersArray)
-console.log(submittedAnswersArray)
+logAnswer(submittedAnswersArray);
+console.log(submittedAnswersArray);
 
 function logAnswer(submittedAnswersArray){
     for (i=0; i<=5; i++){
         if(i === 0){
             if(submittedAnswersArray[0] !== undefined){
-            console.log(submittedAnswersArray[0])    
+            console.log(submittedAnswersArray[0]);    
             whereToLog[0].innerText = submittedAnswersArray[0];
             whereToChange[0].className = howDidYouDo(correctAnswer, submittedAnswersArray[0]);
           }  
         } else if(i === 1){
             if(submittedAnswersArray[1] !== undefined){
-            console.log(submittedAnswersArray[1])    
+            console.log(submittedAnswersArray[1]);    
             whereToLog[0].innerText =submittedAnswersArray[1];
             whereToChange[0].className = howDidYouDo(correctAnswer, submittedAnswersArray[1]);
           }  
         } else if(i === 2){
             if(submittedAnswersArray[2] !== undefined){
-            console.log(submittedAnswersArray[2])    
+            console.log(submittedAnswersArray[2]);   
             whereToLog[0].innerText =submittedAnswersArray[2];
             whereToChange[0].className = howDidYouDo(correctAnswer, submittedAnswersArray[2]);
           }  
         } else if(i === 3){
             if(submittedAnswersArray[3] !== undefined){
-            console.log(submittedAnswersArray[3])    
+            console.log(submittedAnswersArray[3]); 
             whereToLog[0].innerText =submittedAnswersArray[3];
             whereToChange[0].className = howDidYouDo(correctAnswer, submittedAnswersArray[3]);
           }  
         } else if(i === 4){
             if(submittedAnswersArray[4] !== undefined){
-            console.log(submittedAnswersArray[4])    
+            console.log(submittedAnswersArray[4]);    
             whereToLog[0].innerText =submittedAnswersArray[4];
             whereToChange[0].className = howDidYouDo(correctAnswer, submittedAnswersArray[4]);
           }  
         } else if (i === 5){
             if(submittedAnswersArray[5] !== undefined){
-            console.log(submittedAnswersArray[5])    
+            console.log(submittedAnswersArray[5]);   
             whereToLog[0].innerText =submittedAnswersArray[5];
             whereToChange[0].className = howDidYouDo(correctAnswer, submittedAnswersArray[5]);
           }  
@@ -116,19 +115,18 @@ function logAnswer(submittedAnswersArray){
 
 function howDidYouDo(correctAnswer, submittedAnswersArrayIndex){
     
-    let color;
     if (submittedAnswersArrayIndex === correctAnswer){
-        result.innerText = "Winner!!"
-        alert("Winner")
-        return "green"
+        result.innerText = "Winner!!";
+        alert("Winner");
+        return "green";
     }
     else if (submittedAnswersArrayIndex >= (correctAnswer - 5) && submittedAnswersArrayIndex <= (correctAnswer + 5)){
         result.innerText = "You are within 5!!";
-        return "green"
+        return "green";
     }
     else if (submittedAnswersArrayIndex >= (correctAnswer - 15) && submittedAnswersArrayIndex <= (correctAnswer + 15)){
         result.innerText = "You are within 15";
-        return "yellow"
+        return "yellow";
     }
     else if (submittedAnswersArrayIndex < (correctAnswer - 15) || submittedAnswersArrayIndex > (correctAnswer + 15)){ 
         result.innerText = "Not even close.";
